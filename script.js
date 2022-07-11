5c767c72c9694963e33340cbaa540e59b842b34e
Jirakit Jirapongwanich <55320034+jamejunj@users.noreply.github.com>
2022-07-11 23:41:16 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,75 @@ tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+const charList = [
+    {
+        name: 'Fatui',
+        vid: 'TmaAOV4SJNQ',
+        sid: '34jc0Y4bzT0',
+        vision: 'None',
+    },
+    {
+        name: 'Shikanoin Heizou',
+        vid: 'mp-u0LmUikU',
+        sid: '2dFZF056pkc',
+        vision: 'Anemo',
+    },
+    {
+        name: 'Kuki Shinobu',
+        vid: '13fviLTc0Aw',
+        sid: '8pei2OjUc40',
+        vision: 'Electro',
+    },
+    {
+        name: 'Kazuha',
+        vid: 'zif0Lmhrivc',
+        sid: 'ZxLLysD9q6g',
+        vision: 'Pyro',
+    },
+    {
+        name: 'Klee',
+        vid: 'C_duDk5e8yU',
+        sid: '7-VnFQvCLDc',
+        vision: 'Pyro',
+    },
+    {
+        name: 'Zhongli',
+        vid: '4oBpaBEMBIM',
+        sid: 'wO3_S82III0',
+        vision: 'Geo',
+    },
+    {
+        name: 'Raiden Shogun',
+        vid: 'mvrW4aKwAXw',
+        sid: 'iFg-bFAu2AU',
+        vision: 'Electro',
+    },
+    {
+        name: 'Hu Tao',
+        vid: 'qrH9vMZBwAk',
+        sid: 'vRj3YbsVTPc',
+        vision: 'Pyro',
+    },
+    {
+        name: 'Ganyu',
+        vid: 'FHRXP8ceeBU',
+        sid: '_yZTVFSzJC8',
+        vision: 'Cryo',
+    },
+    {
+        name: 'Xiao',
+        vid: 'sjozpa9DsZU',
+        sid: '-tCIg3NQh6Y',
+        vision: 'Anemo',
+    },
+    {
+        name: 'Venti',
+        vid: 't6BZjmGpq40',
+        sid: '0MiIciljaWY',
+        vision: 'Anemo',
+    }
+]
+
 class QueryString{
     #query;
     constructor(df=true){
@@ -33,8 +102,8 @@ var player;
 var sound;
 function onYouTubeIframeAPIReady() {
     if (query.isset('play') && query.isset('v') && query.isset('s')){
-        createPlayer(query.get('v'), query.get('s'))
-        document.querySelector('.form').remove();
+        createPlayer(query.get('v'), query.get('s'), (query.isset('lang') ? query.get('lang'): 'en'));
+        if (query.isset('share')) document.querySelector('.form').remove();
     }
 }
 
@@ -47,14 +116,22 @@ function youtube_parser(url){
 function load(){
     vid = youtube_parser(document.querySelector('input[name=video]').value);
     sid = youtube_parser(document.querySelector('input[name=sound]').value);
-    if (vid && sid) createPlayer(vid, sid);
-    else {
+    if (vid && sid){
+        if (player && sound){
+            change(vid, sid);
+        }else{
+            createPlayer(vid, sid);
+        }
+        document.querySelector('input[name=video]').style.borderColor = '';
+        document.querySelector('input[name=sound]').style.borderColor = '';
+    }else {
         if (!vid) document.querySelector('input[name=video]').style.borderColor = 'red';
         if (!sid) document.querySelector('input[name=sound]').style.borderColor = 'red';
     }
+    console.log(vid, sid);
 }
 
-function createPlayer(vid, sid){
+function createPlayer(vid, sid, lang='en'){
     player = new YT.Player('player', {
         height: '390',
         width: '640',
@@ -62,8 +139,10 @@ function createPlayer(vid, sid){
         playerVars: {
           'playsinline': 1,
           'controls': 1,
+          'cc_lang_pref': lang,
         },
         events: {
+          'onReady': onReady,
           'onStateChange': onPlayerStateChange,
         }
       });
@@ -76,15 +155,20 @@ function createPlayer(vid, sid){
             'controls': 0,
           },
     }); 
-    document.querySelector('input[name=share]').value = window.location.origin + window.location.pathname + `?play&v=${vid}&s=${sid}`;
+    document.querySelector('input[name=share]').value = window.location.origin + window.location.pathname + `?play&v=${vid}&s=${sid}&share`;
 }
 
 function play() {
+    sound.setVolume(player.getVolume());
     player.mute();
     sound.playVideo();
     interval = setInterval(() => {
         if (Math.abs(player.getCurrentTime() - sound.getCurrentTime()) > 1) {
             sound.seekTo(player.getCurrentTime());
+        }
+        if (!player.isMuted()){
+            sound.setVolume(player.getVolume());
+            player.mute();
         }
     },500)
 }
@@ -100,4 +184,63 @@ function onPlayerStateChange(event) {
         sound.pauseVideo();
         clearInterval(interval);
     }
+}
+
+function onReady(event){
+    document.title = player.getVideoData().title;
+}
+
+function change(vid, sid){
+    player.loadVideoById(vid);
+    sound.loadVideoById(sid);
+    document.querySelector('input[name=share]').value = window.location.origin + window.location.pathname + `?play&v=${vid}&s=${sid}&share`;
+}
+
+const container = document.querySelector('#char-container')
+charList.forEach(ch=>{
+    appendCharacter(ch);
+})
+
+// if (!query.isset('share')){
+//     document.getElementById('recommanded').remove();
+// }
+
+function appendCharacter(character){
+    const li = document.createElement('li');
+    li.className = 'w-1/4 text-xl rounded m-2 px-2 py-4 cursor-pointer ';
+    switch (character.vision){
+        case 'Anemo':
+            li.className += 'bg-green-200 hover:bg-green-300';
+            break;
+        case 'Geo':
+            li.className += 'bg-amber-200 hover:bg-amber-300'; 
+            break;
+        case 'Hydro':
+            li.className += 'bg-blue-200 hover:bg-blue-300';
+            break;
+        case 'Electro':
+            li.className += 'bg-pink-200 hover:bg-pink-300';
+            break;
+        case 'Cryo':
+            li.className += 'bg-sky-200 hover:bg-sky-300';
+            break;
+        case 'Pyro':
+            li.className += 'bg-orange-200 hover:bg-orange-300';
+            break;
+        case 'Dendro':
+            li.className += 'bg-lime-200 hover:bg-lime-300';
+            break;
+        default:
+            li.className += 'bg-gray-200 hover:bg-gray-300';
+            break;
+    }
+    li.onclick = () =>{
+        if (player && sound){
+            change(character.vid, character.sid);
+        }else{
+            createPlayer(character.vid, character.sid);
+        }
+    }
+    li.innerText = character.name;
+    container.append(li)
 }
